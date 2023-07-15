b283c9554b1da1846deca61c5f8d8c1ea0084970
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-07-15 12:06:06 +0700

    Date[15-jul-2023]

---
+++ components/CustomText.js
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyCustomTextWith = ({fname, lname}) => {
+    return(
+        <View>
+            <Text>Your First Name is {fname}! and Last name is {lname}</Text>
+        </View>
+    )
+}
+
+const CustomText = () => {
+  return (
+    <View>
+      <MyCustomTextWith fname="Kanlayanuch" lname="Srichote"/>
+      <MyCustomTextWith fname="Naarmorn" lname="Supapsoontorn"/>
+    </View>
+  )
+}
+
+export default CustomText
