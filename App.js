f8c223d31c695c6f4ba2a9544b586181bf6a7999
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-07-29 11:48:19 +0700

    Second Commit [Week5-July-29-2023]

--- App.js
+++ App.js
@@ -1,36 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
-import ViewBoxesWithColorAndText from "./components/ViewBoxesWithColorAndText";
-import DisplayandImage from "./components/DisplayandImage";
-import LotsOfGreeting from "./components/LotsOfGreeting";
-import CustomText from "./components/CustomText";
-import Counter from "./components/Counter";
-import MyInput from "./components/MyInput";
-import IncrementCounter from "./components/IncrementCounter";
-import Form from "./components/Form";
-import Login from "./components/Login";
-import Recap from "./components/Recap";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TouchableExample from './components/TouchableExample'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      {/* <ViewBoxesWithColorAndText/> */}
-      {/* <DisplayandImage/> */}
-      {/* <LotsOfGreeting/> */}
-      {/* <CustomText/> */}
-      {/* <Counter/> */}
-      {/* <MyInput/> */}
-      {/* <IncrementCounter/> */}
-      {/* <Form/> */}
-      {/* <Login/> */}
-      <Recap/>
+      <TouchableExample/>
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container:{
-     flex:1,   
-     justifyContent:'center',    
-     alignItems:'center'   
-  }
- })
+    flex:1,   
+    justifyContent:'center',    
+    alignItems:'center'   
+ }
+})
