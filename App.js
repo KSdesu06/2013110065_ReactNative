d3ea2fe2394d9fa97886bf1478920cecc0a384f0
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-08-05 11:02:09 +0700

    Example_useEffect[week6-05-Aug-2023]

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import TouchableExample from './components/TouchableExample'
 import TouchablePractice from './components/TouchablePractice'
+import Example_useEffect from './components/Example_useEffect'
 
 const App = () => {
   return (
     <View style={styles.container}>
       {/* <TouchableExample/> */}
-      <TouchablePractice/>
+      {/* <TouchablePractice/> */}
+      <Example_useEffect/>
     </View>
   )
 }
@@ -17,7 +19,7 @@ export default App
 const styles = StyleSheet.create({
   container:{
     flex:1,   
-    justifyContent:'center',    
-    alignItems:'center'   
+    justifyContent:'center',   
+    // alignItems:'center'   
  }
 })
