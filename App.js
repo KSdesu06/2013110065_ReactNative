efb3f5d98a741e4c5beac207b0a6ff85d47a9a84
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-08-19 11:46:18 +0700

    Second Commit [Lecture7-August-19-2023]

--- App.js
+++ App.js
@@ -5,15 +5,19 @@ import TouchablePractice from './components/TouchablePractice'
 import Example_useEffect from './components/Example_useEffect'
 import UseEffectFlatList from './components/UseEffectFlatList'
 import RandomUsersScreen from './components/RandomUsersScreen'
+import FlatList_Example1 from './components/FlatList_Example1'
+import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       {/* <TouchableExample/> */}
       {/* <TouchablePractice/> */}
       {/* <Example_useEffect/> */}
       {/* <UseEffectFlatList/> */}
-      <RandomUsersScreen/>
+      {/* <RandomUsersScreen/> */}
+      {/* <FlatList_Example1/> */}
+      <FlatList_HeaderFooter/>
     </View>
   )
 }
@@ -23,7 +27,7 @@ export default App
 const styles = StyleSheet.create({
   container:{
     flex:1,   
-    justifyContent:'center',   
-    alignItems:'center'   
+    justifyContent:'left',   
+    alignItems:'left'   
  }
 })
