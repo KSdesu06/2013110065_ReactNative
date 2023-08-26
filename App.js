92ef6593fefcff0d712aee8c7737c10b9d8c830b
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-08-26 11:22:15 +0700

    First Commit [Lecture8-August-26-2023]

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import TouchableExample from './components/TouchableExample'
-import TouchablePractice from './components/TouchablePractice'
-import Example_useEffect from './components/Example_useEffect'
-import UseEffectFlatList from './components/UseEffectFlatList'
-import RandomUsersScreen from './components/RandomUsersScreen'
-import FlatList_Example1 from './components/FlatList_Example1'
-import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
+import FlatListAPI from './components/FlatListAPI'
+import News from './components/News'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <TouchableExample/> */}
-      {/* <TouchablePractice/> */}
-      {/* <Example_useEffect/> */}
-      {/* <UseEffectFlatList/> */}
-      {/* <RandomUsersScreen/> */}
-      {/* <FlatList_Example1/> */}
-      <FlatList_HeaderFooter/>
+    <View>
+      {/* <FlatListAPI/> */}
+      <News/>
     </View>
   )
 }
@@ -27,7 +17,7 @@ export default App
 const styles = StyleSheet.create({
   container:{
     flex:1,   
-    justifyContent:'left',   
-    alignItems:'left'   
+    justifyContent:'center',   
+    alignItems:'center'   
  }
 })
