b698b9bf58dc15799a3dfd680a736ac6d9185f2c
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-09-02 11:09:22 +0700

    Second Commit [Lecture8-September-02-2023] Practice

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation}) => {
   return (
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
       <Text>Detail Screen</Text>
+      <Text>{"\n"}</Text>
+      <Button
+        title="Go to Details...again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Text>{"\n"}</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+      <Text>{"\n"}</Text>
+      <Button
+        title="Go Back"
+        onPress={() => navigation.goBack()}
+      />
     </View>
   )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,17 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from "react-native";
+import React from "react";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-        <Text>Home Screen</Text>
-        <Button
-        title='Go to Details'
-        onPress={() => navigation.navigate('Details')}
-        />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Text>{"\n"}</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
