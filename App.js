28d3cd6a937bca98b9d4dcea553e5378c6001dee
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-09-09 11:51:21 +0700

    Second Commit [Lecture9-September-09-2023]

--- App.js
+++ App.js
@@ -1,34 +1,81 @@
+import "react-native-gesture-handler";
 import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import CreatePostScreen from "./screens/CreatePostScreen";
-import IndexScreen from "./screens/IndexScreen";
 
+import FirstPage from "./pages/FirstPage";
+import SecondPage from "./pages/SecondPage";
+
+function FirstScreenStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#008b8b",
+        },
+        headerTintColor: "#ffff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="FirstPage"
+        component={FirstPage}
+        options={{ title: "First Page" }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function SecondScreenStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#008b8b",
+        },
+        headerTintColor: "#ffff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="SecondPage"
+        component={SecondPage}
+        options={{ title: "Second Page" }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="FirstDrawer"
+        component={FirstScreenStack}
+        options={{ drawerLabel: "First page Option" }}
+      />
+      <Drawer.Screen
+        name="SecondDrawer"
+        component={SecondScreenStack}
+        options={{ drawerLabel: "Second page Option" }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        mode="model"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#008b8b",
-          },
-          headerTintColor: "#ffff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="Index"
-          component={IndexScreen}
-          options={{ title: "MainPage" }}
-        />
-        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
-      </Stack.Navigator>
+      <MyDrawer />
     </NavigationContainer>
   );
 };
--- App_Navigator.js
+++ App_Navigator.js
@@ -1,45 +1,43 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function HomeScreen({navigation}){
-  return(
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-        <Text>Home Screen</Text>
-        <Button
-        title='Go to Details'
-        onPress={() => navigation.navigate('Details')}
-        />
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
-};
- 
-function DetailsScreen(){
-  return(
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Detail Screen</Text>
     </View>
   );
-};
+}
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-   <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
-        name='Home'
-        component={HomeScreen}
-        options={{title:'Overview'}}
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Overview" }}
         />
-        <Stack.Screen name='Details' component={DetailsScreen}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
-   </NavigationContainer>
+    </NavigationContainer>
   );
 };
 
- 
-
-export default App
+export default App;
